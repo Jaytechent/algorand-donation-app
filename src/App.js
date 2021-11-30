ca0cd6d88f4a90fe9ae5a414b88e5cebc8e99acf
Jaytechent <91403188+Jaytechent@users.noreply.github.com>
2021-11-30 08:09:37 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { Component } from 'react';
 import { Card } from 'pipeline-ui';
-import AlgoDonate from './AlgoDonate';
+import AlgoDonate from './AlgoDonation';
 
 class App extends Component {
   render() {
@@ -10,7 +10,7 @@ class App extends Component {
         <Card bg={"#e8e8e8"} width={"auto"} maxWidth={"750px"} mx={"auto"} my={3} p={0} pb={3} px={3}>
           {/* Success Message & Output Transaction ID */}
           <AlgoDonate 
-            receiverAddress="FDASBUDD5B4YJEBZAYKWGFMF2VS7R2HC7A6UV52GGGLY6FNYDMOHXPYEJU"
+            receiverAddress="2OWDII7AOVTNHA4YRFUGTUITOW62UAAQ7L7WJX7AXY34JZVBYFU65T7MV4"
           />
         </Card>
         
